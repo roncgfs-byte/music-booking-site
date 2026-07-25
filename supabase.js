b2fb9dcc1408515a0b318f052f50c48119d14589
roncgfs-byte <roncgfs@gmail.com>
2026-07-25 16:29:17 -0700

    Change Supabase anonymous API key
    Updated the Supabase anonymous key to a new value.

--- supabase.js
+++ supabase.js
@@ -7,7 +7,7 @@
 const SUPABASE_URL = "https://oyeopxbtdrvtmdedsoli.supabase.co";
 
 // Your Publishable API Key
-const SUPABASE_ANON_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_ANON_KEY = "sb_publishable_1eq1XDcLRotp2UDxg-WuAQ_ez0l30oW";
 
 // Create Supabase Client
 const supabaseClient = supabase.createClient(
